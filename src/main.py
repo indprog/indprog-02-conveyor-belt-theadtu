e6e60b2d97bca0a61bef80cbd45a33c0aa1618ad
Thedtua <s245927@student.dtu.dk>
2026-09-08 16:52:03 +0200

    Complete conveyor belt exercise

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello!")
+motors = int(input("How many motors are carrying the packages? "))
+weight = int(input("How many kg of packages do we expect? "))
+
+if weight / motors <= 12:
+    print("Yes! The conveyor belt can carry the packages.")
+else:
+    print("No. The conveyor belt cannot carry the packages.")
